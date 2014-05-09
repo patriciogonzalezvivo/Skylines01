828479799a06dc4679a7139310fae474cd22ca36
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-05-08 22:39:46 -0400

    time in right place

--- VectorCam/src/testApp.cpp
+++ VectorCam/src/testApp.cpp
@@ -17,91 +17,95 @@ void testApp::setup() {
 }
 
 void testApp::update() {
-    counter++;
     
 #ifdef TARGET_RASPBERRY_PI
     frame = cam.grab();
-    if(!frame.empty() && counter > 100 ){
+    if(!frame.empty()){
 #else
 	cam.update();
-	if(cam.isFrameNew() && counter > 100){
+	if(cam.isFrameNew()){
         cv::Mat vcCam = toCv(cam);
 		convertColor(vcCam, frame, CV_RGB2GRAY);
 #endif
 		Canny(frame, edge, CannyThreshold1*2.0, CannyThreshold2*2.0, 5);
-        toOf(edge, pixels);
         
-        vector<ofPolyline> tmp;
+        counter++;
         
-        if(bHough){
-            vector<Vec4i> cvlines;
-            HoughLinesP(edge, cvlines, 1,(PI/180),1,CannyMinGapLength,CannyMinPathLength);
-            for( size_t i = 0; i < cvlines.size(); i++ ){
-                ofPolyline line;
-                line.addVertex(ofPoint(cvlines[i][0],cvlines[i][1]));
-                line.addVertex(ofPoint(cvlines[i][2],cvlines[i][3]));
-                tmp.push_back(line);
-            }
-        } else {
-            tmp = getPaths(pixels, HoughtMinLinLenght, HoughtMaxLineGap);
-        }
-        
-        lines.clear();
-        for (int i = 0; i < tmp.size(); i++) {
-            ofPolyline tmpSpaced;
-            ofPolyline tmpSmooth;
+        if (counter > 50){
+            toOf(edge, pixels);
             
-            if (space>0.0){
-                tmpSpaced = tmp[i].getResampledBySpacing(space);
-            } else {
-                tmpSpaced = tmp[i];
-            }
+            vector<ofPolyline> tmp;
             
-            if (smooth>0.0){
-                tmpSmooth = tmpSpaced.getSmoothed(smooth);
-            } else {
-                tmpSmooth = tmpSpaced;
-            }
-            
-            lines.push_back(tmpSmooth);
-        }
-        
-        
-        {
-            ofxXmlSettings XML;
-            XML.addTag("svg");
-            XML.addAttribute("svg", "version", "1.2",0);
-            XML.addAttribute("svg", "baseProfile", "tiny",0);
-            XML.addAttribute("svg", "id", "Layer_1",0);
-            XML.addAttribute("svg", "xmlns", "http://www.w3.org/2000/svg",0);
-            XML.addAttribute("svg", "xmlns:xlink", "http://www.w3.org/1999/xlink",0);
-            XML.addAttribute("svg", "x", "0px",0);
-            XML.addAttribute("svg", "y", "0px",0);
-            XML.addAttribute("svg", "width", ofToString(width)+"px",0);
-            XML.addAttribute("svg", "height", ofToString(height)+"px",0);
-            XML.addAttribute("svg", "viewBox", "0 0 "+ofToString(width)+ " " +ofToString(height), 0);
-            XML.addAttribute("svg", "xml:space", "preserve",0);
-            
-            XML.pushTag("svg");
-            for (int j = 0; j < lines.size(); j++) {
-                XML.addTag("path");
-                string path = "";
-                for (int i = 0; i < lines[j].size(); i++) {
-                    if(i==0){
-                        path+="M";
-                    } else {
-                        path+="L";
-                    }
-                    path+=ofToString(lines[j][i].x)+","+ofToString(lines[j][i].y);
+            if(bHough){
+                vector<Vec4i> cvlines;
+                HoughLinesP(edge, cvlines, 1,(PI/180),1,CannyMinGapLength,CannyMinPathLength);
+                for( size_t i = 0; i < cvlines.size(); i++ ){
+                    ofPolyline line;
+                    line.addVertex(ofPoint(cvlines[i][0],cvlines[i][1]));
+                    line.addVertex(ofPoint(cvlines[i][2],cvlines[i][3]));
+                    tmp.push_back(line);
                 }
-                XML.addAttribute("path","d",path,j);
-                XML.addAttribute("path","fill","none",j);
-                XML.addAttribute("path","stroke","black",j);
-                XML.addAttribute("path","stroke-width","1",j);
+            } else {
+                tmp = getPaths(pixels, HoughtMinLinLenght, HoughtMaxLineGap);
             }
             
-            if (XML.save("test.svg")){
-                ofExit();
+            lines.clear();
+            for (int i = 0; i < tmp.size(); i++) {
+                ofPolyline tmpSpaced;
+                ofPolyline tmpSmooth;
+                
+                if (space>0.0){
+                    tmpSpaced = tmp[i].getResampledBySpacing(space);
+                } else {
+                    tmpSpaced = tmp[i];
+                }
+                
+                if (smooth>0.0){
+                    tmpSmooth = tmpSpaced.getSmoothed(smooth);
+                } else {
+                    tmpSmooth = tmpSpaced;
+                }
+                
+                lines.push_back(tmpSmooth);
+            }
+            
+            
+            {
+                ofxXmlSettings XML;
+                XML.addTag("svg");
+                XML.addAttribute("svg", "version", "1.2",0);
+                XML.addAttribute("svg", "baseProfile", "tiny",0);
+                XML.addAttribute("svg", "id", "Layer_1",0);
+                XML.addAttribute("svg", "xmlns", "http://www.w3.org/2000/svg",0);
+                XML.addAttribute("svg", "xmlns:xlink", "http://www.w3.org/1999/xlink",0);
+                XML.addAttribute("svg", "x", "0px",0);
+                XML.addAttribute("svg", "y", "0px",0);
+                XML.addAttribute("svg", "width", ofToString(width)+"px",0);
+                XML.addAttribute("svg", "height", ofToString(height)+"px",0);
+                XML.addAttribute("svg", "viewBox", "0 0 "+ofToString(width)+ " " +ofToString(height), 0);
+                XML.addAttribute("svg", "xml:space", "preserve",0);
+                
+                XML.pushTag("svg");
+                for (int j = 0; j < lines.size(); j++) {
+                    XML.addTag("path");
+                    string path = "";
+                    for (int i = 0; i < lines[j].size(); i++) {
+                        if(i==0){
+                            path+="M";
+                        } else {
+                            path+="L";
+                        }
+                        path+=ofToString(lines[j][i].x)+","+ofToString(lines[j][i].y);
+                    }
+                    XML.addAttribute("path","d",path,j);
+                    XML.addAttribute("path","fill","none",j);
+                    XML.addAttribute("path","stroke","black",j);
+                    XML.addAttribute("path","stroke-width","1",j);
+                }
+                
+                if (XML.save("test.svg")){
+                    ofExit();
+                }
             }
         }
 	}
@@ -161,7 +165,11 @@ vector<ofPolyline> testApp::getPaths(ofPixels& img, float minGapLength, int minP
 void testApp::draw() {
     ofBackground(0);
     
+#ifdef TARGET_RASPBERRY_PI
     if(!frame.empty()){
+#else
+    {
+#endif
         drawMat(edge,0, 0);
     }
 }
